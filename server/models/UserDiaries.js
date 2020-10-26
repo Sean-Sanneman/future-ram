43d8359386a7da0934cc57c3652fd204359b2cfd
unknown <pv95335@gmail.com>
2020-10-26 12:58:12 -0700

    combined backend and Zenquote

---
+++ server/models/UserDiaries.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('brcrypt');
+const Diaries = require('./Diaries');
+
+// import schema from User.js
+const userDiariesSchema = new Schema(
+
+)
